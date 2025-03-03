7b73ccef86ca2c8bf1e55d1762c80b54e30d13e9
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-03-02 19:59:47 -0700

    Update DDL.sql

--- scripts/silver/DDL.sql
+++ scripts/silver/DDL.sql
@@ -9,7 +9,7 @@ create Table silver.crm_cust_info (
 	cst_material_status VARCHAR(50),
 	cst_gndr VARCHAR(50),
 	cst_create_date DATE,
-	dwh_create_date TIMESTAMP
+	dwh_create_date TIMESTAMP default current_timestamp
 );
 Drop table if exists silver.crm_prd_info;
 create Table silver.crm_prd_info (
@@ -20,7 +20,7 @@ create Table silver.crm_prd_info (
     prd_line     VARCHAR(50),
     prd_start_dt DATE,
     prd_end_dt   DATE,
-	dwh_create_date TIMESTAMP
+	dwh_create_date TIMESTAMP default current_timestamp
 );
 Drop table if exists silver.crm_sales_details;
 
@@ -34,7 +34,7 @@ create Table silver.crm_sales_details (
     sls_sales    INT,
     sls_quantity INT,
     sls_price    INT,
-	dwh_create_date TIMESTAMP
+	dwh_create_date TIMESTAMP default current_timestamp
 );
 Drop table if exists silver.erp_cust_az12;
 
@@ -42,7 +42,7 @@ create Table silver.erp_cust_az12(
 	cid    VARCHAR(50),
     bdate  DATE,
     gen    VARCHAR(50),
-	dwh_create_date TIMESTAMP
+	dwh_create_date TIMESTAMP default current_timestamp
 );
 
 Drop table if exists silver.erp_loc_a101;
@@ -50,7 +50,7 @@ Drop table if exists silver.erp_loc_a101;
 create Table silver.erp_loc_a101(
 	cid    VARCHAR(50),
     cntry  VARCHAR(50),
-	dwh_create_date TIMESTAMP
+	dwh_create_date TIMESTAMP default current_timestamp
 );
 
 Drop table if exists silver.px_cat_g1v2;
@@ -60,7 +60,7 @@ create Table silver.px_cat_g1v2(
     cat          VARCHAR(50),
     subcat       VARCHAR(50),
     maintenance  VARCHAR(50),
-	dwh_create_date TIMESTAMP
+	dwh_create_date TIMESTAMP default current_timestamp
 );
 
 -- Select * from silver.px_cat_g1v2;
